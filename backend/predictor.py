637d1500da2dd4874feb8cb1110ea55a511fe0fd
chittireddyvarsha20-spec <chittireddyvarsha20@gmail.com>
2026-04-16 20:51:59 +0530

    Update predictor.py

--- backend/predictor.py
+++ backend/predictor.py
@@ -1,12 +1,17 @@
+import os
 import pickle
 
-# load trained model
-model = pickle.load(open("../ml_model/model.pkl","rb"))
+# get current file directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# correct model path
+model_path = os.path.join(BASE_DIR, "model.pkl")
+
+# load model
+model = pickle.load(open(model_path, "rb"))
+
 
 def predict(features):
-
     prediction = model.predict([features])[0]
-
     confidence = max(model.predict_proba([features])[0])
-
     return prediction, confidence
